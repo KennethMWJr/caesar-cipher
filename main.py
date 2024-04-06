a89ba55504fa1cd20cd6efd6b00f036f6afc9f94
Kenneth Williams <kwilliam08@gmail.com>
2024-04-06 12:50:53 -0400

    setting up logic

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+file_path = '/index.html'
+response = requests.get('file://' + file_path)
+html_content = response.text
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
 def test():
     doll = 1 + 1
